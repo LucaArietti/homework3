6fe94f9bcc13aaec61766d257885de3c65bde539
lucaarietti <ariets.luca@gmail.com>
2018-06-08 12:23:21 +0200

    funziona, crea la mappa, ma non puo esser letta da pcl_viewer perche manca l'intestazione

---
+++ Pangolin/build/src/CMakeFiles/pangolin.dir/DependInfo.cmake
@@ -0,0 +1,100 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/luca/git/Pangolin/src/display/device/display_wayland.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/device/display_wayland.cpp.o"
+  "/home/luca/git/Pangolin/src/display/device/display_x11.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/device/display_x11.cpp.o"
+  "/home/luca/git/Pangolin/src/display/display.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/display.cpp.o"
+  "/home/luca/git/Pangolin/src/display/image_view.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/image_view.cpp.o"
+  "/home/luca/git/Pangolin/src/display/opengl_render_state.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/opengl_render_state.cpp.o"
+  "/home/luca/git/Pangolin/src/display/view.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/view.cpp.o"
+  "/home/luca/git/Pangolin/src/display/viewport.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/viewport.cpp.o"
+  "/home/luca/git/Pangolin/src/display/widgets/widgets.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/widgets/widgets.cpp.o"
+  "/home/luca/git/Pangolin/src/display/window_factory.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/display/window_factory.cpp.o"
+  "/home/luca/git/Pangolin/build/src/fonts.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/fonts.cpp.o"
+  "/home/luca/git/Pangolin/src/gl/glchar.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/gl/glchar.cpp.o"
+  "/home/luca/git/Pangolin/src/gl/gldraw.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/gl/gldraw.cpp.o"
+  "/home/luca/git/Pangolin/src/gl/glfont.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/gl/glfont.cpp.o"
+  "/home/luca/git/Pangolin/src/gl/glpangoglu.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/gl/glpangoglu.cpp.o"
+  "/home/luca/git/Pangolin/src/gl/gltext.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/gl/gltext.cpp.o"
+  "/home/luca/git/Pangolin/src/gl/gltexturecache.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/gl/gltexturecache.cpp.o"
+  "/home/luca/git/Pangolin/src/handler/handler.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/handler/handler.cpp.o"
+  "/home/luca/git/Pangolin/src/handler/handler_glbuffer.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/handler/handler_glbuffer.cpp.o"
+  "/home/luca/git/Pangolin/src/handler/handler_image.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/handler/handler_image.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io_exr.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io_exr.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io_jpg.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io_jpg.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io_pango.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io_pango.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io_png.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io_png.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io_ppm.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io_ppm.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io_raw.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io_raw.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io_tga.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io_tga.cpp.o"
+  "/home/luca/git/Pangolin/src/image/image_io_zstd.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/image_io_zstd.cpp.o"
+  "/home/luca/git/Pangolin/src/image/pixel_format.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/image/pixel_format.cpp.o"
+  "/home/luca/git/Pangolin/src/log/packet.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/log/packet.cpp.o"
+  "/home/luca/git/Pangolin/src/log/packetstream.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/log/packetstream.cpp.o"
+  "/home/luca/git/Pangolin/src/log/packetstream_reader.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/log/packetstream_reader.cpp.o"
+  "/home/luca/git/Pangolin/src/log/packetstream_writer.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/log/packetstream_writer.cpp.o"
+  "/home/luca/git/Pangolin/src/log/playback_session.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/log/playback_session.cpp.o"
+  "/home/luca/git/Pangolin/src/plot/datalog.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/plot/datalog.cpp.o"
+  "/home/luca/git/Pangolin/src/plot/plotter.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/plot/plotter.cpp.o"
+  "/home/luca/git/Pangolin/src/tools/video_viewer.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/tools/video_viewer.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/file_extension.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/file_extension.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/file_utils.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/file_utils.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/posix/condition_variable.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/posix/condition_variable.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/posix/semaphore.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/posix/semaphore.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/posix/shared_memory_buffer.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/posix/shared_memory_buffer.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/sigstate.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/sigstate.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/threadedfilebuf.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/threadedfilebuf.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/timer.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/timer.cpp.o"
+  "/home/luca/git/Pangolin/src/utils/uri.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/utils/uri.cpp.o"
+  "/home/luca/git/Pangolin/src/var/input_record_repeat.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/var/input_record_repeat.cpp.o"
+  "/home/luca/git/Pangolin/src/var/vars.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/var/vars.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/debayer.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/debayer.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/images.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/images.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/images_out.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/images_out.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/join.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/join.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/json.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/json.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/merge.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/merge.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/mirror.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/mirror.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/openni.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/openni.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/pango.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/pango.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/pango_video_output.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/pango_video_output.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/pvn.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/pvn.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/shift.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/shift.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/split.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/split.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/test.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/test.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/thread.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/thread.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/unpack.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/unpack.cpp.o"
+  "/home/luca/git/Pangolin/src/video/drivers/v4l.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/drivers/v4l.cpp.o"
+  "/home/luca/git/Pangolin/src/video/stream_encoder_factory.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/stream_encoder_factory.cpp.o"
+  "/home/luca/git/Pangolin/src/video/video.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/video.cpp.o"
+  "/home/luca/git/Pangolin/src/video/video_input.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/video_input.cpp.o"
+  "/home/luca/git/Pangolin/src/video/video_interface_factory.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/video_interface_factory.cpp.o"
+  "/home/luca/git/Pangolin/src/video/video_output.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/video_output.cpp.o"
+  "/home/luca/git/Pangolin/src/video/video_output_interface_factory.cpp" "/home/luca/git/Pangolin/build/src/CMakeFiles/pangolin.dir/video/video_output_interface_factory.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "linux=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "src/include"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/x86_64-linux-gnu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
